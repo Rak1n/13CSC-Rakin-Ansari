9e003681b4150b313614b8dd1b5ed040d73d18fd
22102 <@Rak1n>
2026-05-07 09:52:01 +1200

    i added set geometry and min/max size  to window , while started creating base design.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from tkinter import *
-import tk
+import tkinter as tk
 from PIL import Image, ImageTk
 import random
-
+w = 1920
+h = 1080
 name_list = []
 asked = []
 score = 0
@@ -12,13 +13,13 @@ class Menu:
         background_color = "#e8f9ff"
 
         self.parent = parent
-        self.original_bg_image = Image.open("engin_akyurt-pizza-2766471_1920.jpg")
-        self.bg_photo = ImageTk.PhotoImage(self.original_bg_image)
-        self.bg_label = Label(parent, image=self.bg_photo)
-        self.bg_label.image = self.bg_photo
-        self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+        #self.original_bg_image = Image.open("engin_akyurt-pizza-2766471_1920.jpg")
+        #self.bg_photo = ImageTk.PhotoImage(self.original_bg_image)
+        #self.bg_label = Label(parent, image=self.bg_photo)
+        #self.bg_label.image = self.bg_photo
+        #self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        self.parent.bind("<Configure>", self.resize_bg)
+        #self.parent.bind("<Configure>", self.resize_bg)
 
         self.quiz_frame = Frame(parent, background=background_color)
         self.quiz_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
@@ -27,20 +28,21 @@ class Menu:
         self.content_frame.grid()
 
 
-    def resize_bg(self, event):
-        if event.widget == self.parent:
-            new_image = self.original_bg_image.resize((event.width, event.height))
-            self.bg_photo = ImageTk.PhotoImage(new_image)
-            self.bg_label.config(image=self.bg_photo)
-            self.bg_label.image = self.bg_photo
+    #def resize_bg(self, event):
+     #   if event.widget == self.parent:
+      #      new_image = self.original_bg_image.resize((event.width, event.height))
+       #     self.bg_photo = ImageTk.PhotoImage(new_image)
+        #    self.bg_label.config(image=self.bg_photo)
+         #   self.bg_label.image = self.bg_photo
 
 
 
 
 if __name__ == "__main__":
-        root = Tk()
-        root.minsize(1920,1080)
-        root.maxsize(1700,400)
+        root = tk.Tk()
+        root.geometry("1920x1080")
+        root.minsize(1200,800)
+        root.maxsize(1920,1080)
         root.iconbitmap("Falcon.png")
         root.title("General Knowledge Quiz")
         root.geometry("800x700")  # Optional: fixed starting size
