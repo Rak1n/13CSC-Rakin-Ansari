66f114b10ecf2eb84061a364c9bbadef296b8405
22102 <@Rak1n>
2026-05-08 15:03:30 +1200

    added pack function and started to adjust size

--- main.py
+++ main.py
@@ -10,32 +10,31 @@ score = 0
 
 class Menu:
     def __init__(self, parent):
-        background_color = "#e8f9ff"
+        background_color = "#cc3628"
 
         self.parent = parent
-        #self.original_bg_image = Image.open("engin_akyurt-pizza-2766471_1920.jpg")
-        #self.bg_photo = ImageTk.PhotoImage(self.original_bg_image)
-        #self.bg_label = Label(parent, image=self.bg_photo)
-        #self.bg_label.image = self.bg_photo
-        #self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+        self.original_bg_image = Image.open("engin_akyurt-pizza-2766471_1920.jpg")
+        self.bg_photo = ImageTk.PhotoImage(self.original_bg_image)
+        self.bg_label = Label(parent, image=self.bg_photo)
+        self.bg_label.image = self.bg_photo
+        self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        #self.parent.bind("<Configure>", self.resize_bg)
+        self.parent.bind("<Configure>", self.resize_bg)
 
         self.quiz_frame = Frame(parent, background=background_color)
         self.quiz_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
 
         self.content_frame = Frame(self.quiz_frame, background=background_color)
         self.content_frame.grid()
+        self.bg_label.pack()
 
 
-    #def resize_bg(self, event):
-     #   if event.widget == self.parent:
-      #      new_image = self.original_bg_image.resize((event.width, event.height))
-       #     self.bg_photo = ImageTk.PhotoImage(new_image)
-        #    self.bg_label.config(image=self.bg_photo)
-         #   self.bg_label.image = self.bg_photo
-
-
+    def resize_bg(self, event):
+        if event.widget == self.parent:
+            new_image = self.original_bg_image.resize(width=, height=)
+            self.bg_photo = ImageTk.PhotoImage(new_image)
+            self.bg_label.config(image=self.bg_photo)
+            self.bg_label.image = self.bg_photo
 
 
 if __name__ == "__main__":
