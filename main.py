fd7b293ad53ca54f7a897fc1a7e47fef8822c6e2
22102 <22102@students.mrgs.school.nz>
2026-05-18 13:29:24 +1200

    added side menu buttons

--- main.py
+++ main.py
@@ -16,14 +16,13 @@ class Menu:
 
         self.parent = parent
         self.root = root
-        self.original_bg_image = Image.open("engin_akyurt-pizza-2766471_1920.jpg")
+        self.original_bg_image = Image.open("Screenshot 2026-05-18 124046.png")
         self.bg_photo = ImageTk.PhotoImage(self.original_bg_image)
-        self.bg_label = Label(parent, image=self.bg_photo, width=1920,height=300)
+        self.bg_label = Label(parent, image=self.bg_photo)
         self.bg_label.image = self.bg_photo
         self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        self.header_frame = Label(parent, bg="#153c7d" , width=1920, height=5, borderwidth=10, padx=5)
-        self.header_frame.place(y=304)
+
 
         #self.image2 = Image.open('Falcon.png')
         #self.phoneix = ImageTk.PhotoImage(self.image2)
@@ -34,9 +33,18 @@ class Menu:
 
 
         # Create button and image
-        self.img = Button(parent, borderwidth=0, width=100)
-        self.img.place(x=11, y=100)
+        self.button = PhotoImage(file='button_menu (1).png')
+        self.img = Button(parent, borderwidth=0, width=200, bg="#cc3628" ,image=self.button,activebackground="#cc3628", activeforeground="white")
+        self.img.place(x=51, y=500)
 
+        self.button2 = PhotoImage(file='button_pita.png')
+        self.img2 = Button(parent, borderwidth=0, width=200, bg="#cc3628", image=self.button2, activebackground="#cc3628",activeforeground="white")
+        self.img2.place(x=51, y=600)
+
+        self.button3 = PhotoImage(file='button_specials (5).png')
+        self.img3 = Button(parent, borderwidth=0, width=200, bg="#cc3628", image=self.button3,
+                           activebackground="#cc3628", activeforeground="white")
+        self.img3.place(x=51, y=800)
 
         self.parent.bind("<Configure>", self.resize_bg)
 
@@ -58,13 +66,11 @@ class Menu:
 
 if __name__ == "__main__":
         root = tk.Tk()
-        root.geometry("400x400")
-
+        root.geometry("1920x1080")
         root.minsize(1920,1080)
         root.maxsize(2200,1300)
         root.iconbitmap("Falcon.png")
         root.title("General Knowledge Quiz")
-        root.geometry("800x700")  # Optional: fixed starting size
         root.configure(bg="#800517")
         Menu_object = Menu(root)
         root.mainloop()
